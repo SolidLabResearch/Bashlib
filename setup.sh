5f04569ded01fbff1eff694a7f7bcc37a4a64e23
dexagod <Ruben.Dedecker@Ugent.be>
2022-03-23 17:23:46 +0100

    Fix build command for solid-shell to not error setup script

--- setup.sh
+++ setup.sh
@@ -3,19 +3,22 @@
 cd tools/
 
 cd css/
+echo "building /tools/css/"
 npm install;
 npm run build;
 cd ../
 
 cd solid/
+echo "building /tools/solid/"
 npm install;
 npm run build;
 cd ../
 
-# cd solid-shell/
-# npm install;
-# npm run build;
-# cd ../
+cd solid-shell/
+echo "building /tools/solid-shell/"
+npm install;
+npm run build;
+cd ../
 
 # Exit packages folder
 cd ..
