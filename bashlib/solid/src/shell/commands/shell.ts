bec44fb71e760900f88a3d8427053984ae8967ef
dexagod <Ruben.Dedecker@Ugent.be>
2022-08-02 16:22:41 +0200

    Fix: Remove shell command from options as it's not ready yet

--- bashlib/solid/src/shell/commands/shell.ts
+++ bashlib/solid/src/shell/commands/shell.ts
@@ -6,20 +6,20 @@ import SolidCommand from './SolidCommand';
 export default class ShellCommand extends SolidCommand { 
 
   public addCommand(program: Command) {
-    this.programopts = program.opts();
+    // this.programopts = program.opts();
 
-    program
-      .command('shell')
-      .description('Open a Solid Shell')
-      .action(async () => {
-        try {
-          await shell(program.opts());
-        } catch (e) {
-          writeErrorString(`Could not open Solid Shell`, e)
-          if (this.mayExit) process.exit(1)
-        }
-        if (this.mayExit) process.exit(0)
-      })
+    // program
+    //   .command('shell')
+    //   .description('Open a Solid Shell')
+    //   .action(async () => {
+    //     try {
+    //       await shell(program.opts());
+    //     } catch (e) {
+    //       writeErrorString(`Could not open Solid Shell`, e)
+    //       if (this.mayExit) process.exit(1)
+    //     }
+    //     if (this.mayExit) process.exit(0)
+    //   })
 
     return program
   }
