b4d7637650b268e4707e0fa5e1fc7afc2eddd234
dexagod <Ruben.Dedecker@Ugent.be>
2023-03-18 21:30:36 +0100

    Update: update docs

--- src/commands/solid-list.ts
+++ src/commands/solid-list.ts
@@ -15,7 +15,6 @@ export default async function list(url: string, options: ListingOptions) {
   if (!isDirectory(url)) {
     (options.logger || console).error('List can only be called on containers. Please write containers with their trailing slash.')
     throw new Error('List can only be called on containers.');
-    return;
   }
   let dataset = await getSolidDataset(url, { fetch: options.fetch })
   let containedResources = getContainedResourceUrlAll(dataset)
